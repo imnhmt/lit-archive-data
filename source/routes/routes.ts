9337ee82bf1d7e2804332ad9cf2d14665541c471
imnhmt <nhmtu@mmsofts.com>
2021-10-14 18:07:02 +0700

    Add API Merchant & Company

--- source/routes/routes.ts
+++ source/routes/routes.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 
 import { authenticateToken } from '../utils/authenticate';
-import controller from '../controllers/customer_kyc';
+import customerKYCController from '../controllers/customerKYCController';
+import company from '../controllers/companyController';
+import merchant from '../controllers/merchantController';
 
 const router = express.Router();
 
-router.post('/save_image', authenticateToken, controller.saveImage);
-router.post('/get_image', authenticateToken, controller.getImage);
-router.post('/get_all_image', authenticateToken, controller.getAllImages)
-router.post('/delete_image', authenticateToken, controller.deleteImage);
+//----------------- Customer KYC Router -----------------//
+router.post('/customer_kyc/save_image', authenticateToken, customerKYCController.saveImage);
+router.post('/customer_kyc/get_image', authenticateToken, customerKYCController.getImage);
+router.post('/customer_kyc/get_all_image', customerKYCController.getAllImages)
+router.post('/customer_kyc/delete_image', authenticateToken, customerKYCController.deleteImage);
+
+//----------------- Company Router -----------------//
+router.post('/company/save', company.saveImage);
+router.post('/company/get', company.getImage);
+router.post('/company/delete', company.deleteImage);
+
+//----------------- Merchant Router -----------------//
+router.post('/merchant/save', merchant.saveImage);
+router.post('/merchant/get', merchant.getImage);
+router.post('/merchant/delete', merchant.deleteImage);
 
 export = router;
